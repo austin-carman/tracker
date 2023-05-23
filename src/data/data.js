492de88c7483fea86d47bc22b6c350e5dfcea97e
austin-carman <abcarman12@gmail.com>
2023-05-22 16:40:12 -1000

    added more data

--- src/data/data.js
+++ src/data/data.js
@@ -40,7 +40,20 @@ export const jobsData = {
     },
   ],
   reachedOut: [],
-  interview: [],
+  interview: [
+    {
+      jobId: 4,
+      userId: 1,
+      title: "Software Engineer",
+      location: "remote, US",
+      postUrl: "https://loan-street.com/careers/?gh_jid=4000262005",
+      status: "Interview",
+      jobDescription:
+        "Company Overview LoanStreet is the first fully integrated online marketplace that streamlines the process of sharing, managing, and scaling loan trading for credit unions, banks and direct lenders. Our mission is to provide the most efficient, transparent and robust tools for financial institutions of all sizes to manage their balance sheets, connect with partners and effectively share risk.",
+      notes: "These are my notes",
+      lastStatusChange: "05/22/2023",
+    },
+  ],
   offer: [],
   notMovingForward: [],
 };
